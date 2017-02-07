fbff6782dd1ac40bb170cd2fe2aa6e2f7d94ebc1
benzbrake <coding.net@woai.ru>
2017-02-07 15:07:40 +0800

    update freehao123.py

--- freehao123.py
+++ freehao123.py
@@ -2,45 +2,57 @@
 #
 # FileName		: freehao123.py
 # Description		:  freehao123 列表爬虫
-# Version		: 0.1 beta
-# Date			: 2017-01-16
+# Version		: 0.1 beta 2
+# Date			: 2017-01-17
 #
 from bs4 import BeautifulSoup
 import requests
 import time
-def parse_list(url,data=None):
-    info = []
-    wb_data = requests.get(url)
-    Soup = BeautifulSoup(wb_data.text,'lxml')
-    pTitle = Soup.select('body > div#page > div#center > div.col > [id^="post"] > h4 > a')
-    pContent = Soup.select('body > div#page > div#center > div.col > [id^="post"] > .postcontent')
-    pMeta = Soup.select('body > div#page > div#center > div.col > [id^="post"] > .postmeat')
-    pPages = Soup.select('div.wp-pagenavi > a') # 翻页导航
-    for title,content,meta in zip(pTitle,pContent,pMeta):
-        data = {
-            'title':title.get_text(),
-            'content':content.get_text(),
-            'meta':meta.get_text()
+class miniSpider:
+    def __init__(self, starturl):
+        self.starturl = starturl
+        self.results = []
+        self.urls = []
+
+    def result(self):
+        return self.results
+
+    def parse_list(self,url,data=None):
+        info = []
+        wb_data = requests.get(url)
+        Soup = BeautifulSoup(wb_data.text,'lxml')
+        pTitle = Soup.select('body > div#page > div#center > div.col > [id^="post"] > h4 > a')
+        pContent = Soup.select('body > div#page > div#center > div.col > [id^="post"] > .postcontent')
+        pMeta = Soup.select('body > div#page > div#center > div.col > [id^="post"] > .postmeat')
+        pPages = Soup.select('div.wp-pagenavi > a') # 翻页导航
+        for title,content,meta in zip(pTitle,pContent,pMeta):
+            data = {
+                'title':title.get_text(),
+                'content':content.get_text(),
+                'meta':meta.get_text()
+            }
+            info.append(data)
+        result = {
+            'next': pPages,
+            'result': info
         }
-        info.append(data)
-    result = {
-        'next': pPages,
-        'result': info
-    }
-    time.sleep(1)
-    return result
+        time.sleep(1)
+        return result
 
-def parse_freehao(starturl,urls ,results):
-    if starturl not in urls:
-        urls.append(starturl) # 记录URL，防止重复爬取
-        result = parse_list(starturl)
-        if result != None:
-            results.append(result.get('result'))
-            for url in result.get('next'):
-                parse_freehao(url.get('href'),urls,results)
-    return None
+    def parse_cycle(self,starturl):
+        if starturl not in self.urls:
+            self.urls.append(starturl) # 记录URL，防止重复爬取
+            result = self.parse_list(starturl)
+            if result != None:
+                self.results.append(result.get('result'))
+                for url in result.get('next'):
+                    self.parse_cycle(url.get('href'))
+        return None
+    
+    def catch(self):
+        self.parse_cycle(self.starturl)
 
-urls = []
-results = []
-parse_freehao('https://www.freehao123.com/category/vps-zhuji/',urls,results)
-print(results)
+if __name__ == '__main__':
+    freehao123 = miniSpider('https://www.freehao123.com/category/vps-zhuji/')
+    freehao123.catch()
+    print(freehao123.result())
